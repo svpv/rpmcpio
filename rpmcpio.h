7ee99c4d140355030ab6ea96c76a7770090fac3c
Alexey Tourbin <alexey.tourbin@gmail.com>
2016-12-24 17:49:52 +0300

    rpmcpio.c: first working version

---
+++ rpmcpio.h
@@ -0,0 +1,20 @@
+struct rpmcpio *rpmcpio_open(const char *fname);
+
+struct cpioent {
+    unsigned ino;
+    unsigned mode;
+    unsigned uid;
+    unsigned gid;
+    unsigned nlink;
+    unsigned mtime;
+    unsigned size;
+    unsigned dev_major, dev_minor;
+    unsigned rdev_major, rdev_minor;
+    unsigned fnamelen;
+    unsigned checksum;
+    char fname[];
+};
+
+const struct cpioent *rpmcpio_next(struct rpmcpio *cpio);
+
+int rpmcpio_read(struct rpmcpio *cpio, void *buf, int n);
